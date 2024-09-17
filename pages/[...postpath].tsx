0a726dc2c8388cbef1f8880f48e5b03704a65d64
Rashid04564 <mdsbaloch9@gmail.com>
2024-09-17 10:02:06 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -19,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://dev-rashid-test.pantheonsite.io/just-click/` + encodeURI(path as string)
+					`https://dev-rashid-test.pantheonsite.io/just-click/`
 				}`,
 			},
 		};
